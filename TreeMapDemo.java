d8736f54965a1ff62670ca50560c935b46388321
shaikfayazahmad <84577818+shaikfayazahmad@users.noreply.github.com>
2021-07-10 14:42:31 +0530

    Add files via upload

---
+++ TreeMapDemo.java
@@ -0,0 +1,32 @@
+package collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+public class TreeMapDemo {
+	public static void main(String[] args) {
+		//Map<String,String> map=new HashMap<>();
+		Map<String,String> map=new TreeMap<>((obj1,obj2)->{return obj2.compareTo(obj1);});
+		
+		map.put("mykey1", "my value 11111111111");
+		map.put("mykey2", "my value 22222222222");
+		map.put("mykey3", "my value 33333333333");
+		System.out.println(map);
+		
+		System.out.println(map.get("mykey1"));
+		
+		Set<Map.Entry<String,String>> set= map.entrySet();
+		
+		Iterator<Map.Entry<String,String>> iter=set.iterator();
+		while(iter.hasNext()) {
+			Map.Entry<String,String> me=iter.next();
+			System.out.println(me.getKey()+":"+me.getValue());
+		}
+		
+	}
+	
+	
+
+}
